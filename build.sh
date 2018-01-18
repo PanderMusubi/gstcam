b87e6e1d50c327eb2fc3620402bbc03a72c0a3e0
PanderMusubi <pander@users.sourceforge.net>
2018-01-18 14:22:39 +0100

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@ MACHINE=`uname -m`
 DISTRIB=`lsb_release -i|awk -F ':\t' '{print $2}'`
 VERSION=`lsb_release -r|awk -F ':\t' '{print $2}'`
 GSTREAM=`gst-inspect-1.0 --version|grep ^GStreamer|awk '{print $2}'`
+if [ `lspci|grep VGA|grep -i radeon|wc -l` -ne 0 ]; then
+	VDPAU_DRIVER='radeonsi vainfo'
+fi
 
 cd bin
 gcc -Wall -O2 \
